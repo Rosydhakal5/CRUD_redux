7fada365439917c0bd5162ecdc66eefe4d16a37e
Rosy <rosydhakal333@gmail.com>
2024-01-22 11:51:08 +1100

    adding css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Userlisting from './components/Userlisting';
 import Updateuser from './components/Updateuser';
 import Adduser from './components/Adduser';
 import { ToastContainer } from 'react-toastify';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <div className='header'>
+        <Link to = {'/'}>  Home </Link>
+        <Link to = {'/user'}>User</Link>
+      </div>
+
       <Routes>
         <Route path='/' element = {<Home></Home>}></Route>
         <Route path='/user' element = {<Userlisting></Userlisting>}></Route>
@@ -18,7 +24,7 @@ function App() {
       </Routes>
       </BrowserRouter>
       <ToastContainer>
-        </ToastContainer>  s
+        </ToastContainer> 
     </div>
   );
 }
