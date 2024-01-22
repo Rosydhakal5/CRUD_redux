0a8ab056011c287a3077412d82bb88a2b289c7d8
Rosy <rosydhakal333@gmail.com>
2024-01-22 12:07:44 +1100

    Updated userlisting

--- src/components/Userlisting.jsx
+++ src/components/Userlisting.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Userlisting = () => {
   return (
-    <div>Userlisting</div>
+    <div>
+        <div className='card'>
+        <div className='card-header'>
+            <h2> Userlisting</h2>
+        </div>
+        <div className='card-body'>
+            <table className='table table-bordered'>
+            <thead className="bg-dark">
+                <tr>
+                    <td>Code</td> 
+                    <td>Name</td> 
+                    <td>Email</td> 
+                    <td>Phone</td> 
+                    <td>Role</td> 
+                    <td>Action</td> 
+                </tr>  
+            </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
   )
 }
 
